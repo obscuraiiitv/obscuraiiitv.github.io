4bf92c6e7117a7a7e1480c476fe7507296e7ae30
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-04-11 01:17:31 +0530

    Closes: #9

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ const { body } = document;
 sponsors.addEventListener('click', e => {
     body.classList.toggle('show-sponsors');
 });
+
+//navgation
+var header = document.getElementById("active");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
